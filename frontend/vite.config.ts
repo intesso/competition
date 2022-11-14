43e2f5a27d712b1b7e9a1a7a902210dc50c16108
Andi Neck <andi.neck@intesso.com>
2022-11-14 06:43:32 +0100

    fix vite preview

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default ({ mode }) => {
+export default ({ mode }: { mode: string }) => {
   const env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''))
   console.log('vite mode', mode)
   // https://vitejs.dev/config/
@@ -10,13 +10,13 @@ export default ({ mode }) => {
     envPrefix: 'SHARED_',
     plugins: [react()],
     preview: {
-      port: env.SHARED_PORT
+      port: parseInt(env.SHARED_PORT)
     },
     server: {
       // set hmr to false if you want to switch of hot module reload
       // hmr: false,
       hmr: { host: env.SHARED_SERVER },
-      port: env.SHARED_PORT,
+      port: parseInt(env.SHARED_PORT),
       proxy: {
         '/api': {
           target: 'http://localhost:8081/',
