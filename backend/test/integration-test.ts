39402048a1f06779371d689b77b7b760930bda8e
Andi Neck <andi.neck@intesso.com>
2023-02-20 22:53:46 +0100

    update integration tests

--- backend/test/integration-test.ts
+++ backend/test/integration-test.ts
@@ -10,10 +10,16 @@ const workingDir = __dirname
 console.log('newman workingDir', workingDir)
 
 const copyEnv = `cp ${workingDir}/competition-local.postman_environment.json ${workingDir}/tmp.env.json`
-const integrationTests = `newman run ${workingDir}/competition-tests.postman_collection.json -e ${workingDir}/competition-local.postman_environment.json --export-environment ${workingDir}/tmp.env.json`
+const integrationTests1 = `newman run ${workingDir}/competition-tests-single_Adv&ERSO.postman_collection.json -e ${workingDir}/competition-local.postman_environment.json --export-environment ${workingDir}/tmp.env.json`
+const integrationTests2 = `newman run ${workingDir}/competition-tests-single_Beg&Interm.postman_collection.postman_collection.json -e ${workingDir}/competition-local.postman_environment.json --export-environment ${workingDir}/tmp.env.json`
+const integrationTests3 = `newman run ${workingDir}/competition-tests-single_TripleUnder.postman_collection.postman_collection.json -e ${workingDir}/competition-local.postman_environment.json --export-environment ${workingDir}/tmp.env.json`
+const integrationTests4 = `newman run ${workingDir}/competition-tests-team.postman_collection.postman_collection.json -e ${workingDir}/competition-local.postman_environment.json --export-environment ${workingDir}/tmp.env.json`
 
 ;(async function () {
   await spawnAsync(copyEnv)
-  await spawnAsync(integrationTests)
+  await spawnAsync(integrationTests1)
+  await spawnAsync(integrationTests2)
+  await spawnAsync(integrationTests3)
+  await spawnAsync(integrationTests4)
   await db.dispose()
 })()
